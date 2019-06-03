030fe6f84d04cd157c46d085c1641f797e142c0f
mog2512 <mog8030@gmail.com>
2019-06-04 01:19:54 +0900

    - 메인 페이지 태그 검색 -> 검색 페이지에 결과 출력 거의다 만들었습니다. CSS 작업 없이 기능만 넣고 있어요 - 마이 페이지 파일(빈파일) 만들었습니다.(app/views/my_page/index.html.erb). CSS는 assets/stylesheet/my_page

--- snuplan/app/controllers/events_controller.rb
+++ snuplan/app/controllers/events_controller.rb
@@ -2,14 +2,17 @@ class EventsController < ApplicationController
 	
   #일단 액션 세팅	
   def main #메인페이지
+    @tags = Tag.all
   end
 
   def index #검색된 페이지. 태그에 맞는것들만 불러오기
+     render plain: params[:tag]
   end
 
   def show # 상세페이지
 
-  	@event = Event.find(params[:id])
+
+  	#@event = Event.find(params[:id])
   end
 
   def new # 게시물 올리는 페이지
