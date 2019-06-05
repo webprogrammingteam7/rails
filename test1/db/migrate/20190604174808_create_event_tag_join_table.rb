43feb18456db30986a49da208342afea298f9f8a
mog2512 <mog8030@gmail.com>
2019-06-05 10:46:28 +0900

    test1 제거

--- test1/db/migrate/20190604174808_create_event_tag_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateEventTagJoinTable < ActiveRecord::Migration[5.2]
-  def change
-    create_join_table :events, :tags do |t|
-      # t.index [:event_id, :tag_id]
-      # t.index [:tag_id, :event_id]
-    end
-  end
-end
